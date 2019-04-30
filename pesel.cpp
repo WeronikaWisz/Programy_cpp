aededb007543803e9326781db73de65ac80eb732
WeronikaWisz <32033773+WeronikaWisz@users.noreply.github.com>
2019-04-30 09:07:02 +0200

    Add files via upload

---
+++ pesel.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Pesel{
+
+	public:
+		Pesel(string p): pesel{p} {}
+		~Pesel(){}
+		void validatePESEL();
+	private:
+		string pesel;
+
+};
+
+void Pesel::validatePESEL(){
+	int  control;
+	control = (9*(pesel[0]-'0') + 7*(pesel[1]-'0') + 3*(pesel[2]-'0') + (pesel[3]-'0') + 9*(pesel[4]-'0') + 7*(pesel[5]-'0') + 3*(pesel[6]-'0') + (pesel[7]-'0') + 9*(pesel[8]-'0') + 7*(pesel[9]-'0')) % 10;
+	if( pesel.length()!=11 || (pesel[10]-'0')!=control){
+		cerr << "bledny numer pesel" <<endl;
+		return;
+	}
+	cout << "jest ok" << endl;
+	return;
+}
+
+int main(){
+	Pesel p1{"99011907547"};
+	Pesel p2{"12345678900"};
+	Pesel p3{"123"};
+	p1.validatePESEL();
+	p2.validatePESEL();
+return 0;
+}
+	
